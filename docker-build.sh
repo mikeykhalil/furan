de66b487dcd184c7793057009396650341f84d05
Benjamen Keroack <benjamen.keroack@dollarshaveclub.com>
2016-07-21 19:11:33 -0700

    docker-build should fail on error

--- docker-build.sh
+++ docker-build.sh
@@ -3,8 +3,8 @@
 # DO NOT RUN LOCALLY - For docker build only!
 # WILL NUKE YOUR .git !
 
-apk update
-apk add git
+apk update || exit 1
+apk add git || exit 1
 
 if [[ -d ".git" ]]; then
   git log -1 --pretty="format:%h" > VERSION.txt
@@ -15,11 +15,11 @@ else
   echo "unknown" > DESCRIPTION.txt
 fi
 
-./build_protos.sh
+./build_protos.sh || exit 1
 
-go get -v
+go get -v || exit 1
 go build || exit 1
-go install
+go install || exit 1
 
 rm -rf /go/src/*
 
