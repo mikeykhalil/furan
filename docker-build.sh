c7f0304c6be93577ae4faa0a25278c18c48a90ed
Benjamen Keroack <benjamen.keroack@dollarshaveclub.com>
2017-05-17 11:22:16 -0700

    fix build

--- docker-build.sh
+++ docker-build.sh
@@ -9,7 +9,7 @@ apk add git || exit 1
 ./build_protos.sh || exit 1
 
 go get -v || exit 1
-go build -ldflags "-X lib.version=`git rev-parse HEAD` -X lib.description=`git log -1 --pretty='format:%ai %s'`" || exit 1
+go build || exit 1
 go install || exit 1
 
 rm -rf /go/src/*
