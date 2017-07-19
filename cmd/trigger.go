7fbcf4eaa27f5f2c4dfd25e600587bb4a5558b4d
Benjamen Keroack <benjamen.keroack@dollarshaveclub.com>
2017-07-19 09:34:33 -0700

    support tag skipping in trigger cmd

--- cmd/trigger.go
+++ cmd/trigger.go
@@ -46,6 +46,7 @@ func init() {
 	triggerCmd.PersistentFlags().StringVar(&cliBuildRequest.Push.S3.KeyPrefix, "s3-key-prefix", "", "S3 key prefix")
 	triggerCmd.PersistentFlags().StringVar(&tags, "tags", "master", "image tags (optional, comma-delimited)")
 	triggerCmd.PersistentFlags().BoolVar(&cliBuildRequest.Build.TagWithCommitSha, "tag-sha", false, "additionally tag with git commit SHA (optional)")
+	triggerCmd.PersistentFlags().BoolVar(&cliBuildRequest.SkipIfExists, "skip-if-exists", false, "if build already exists at destination, skip build/push (registry: all tags exist, s3: object exists)")
 	RootCmd.AddCommand(triggerCmd)
 }
 
